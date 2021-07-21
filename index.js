53af9abc5a6221a6d7570c1f7c662cfc4ce02f98
Charlie Gerard <annecharlotte.gerard@gmail.com>
2021-07-21 11:28:42 +0200

    test trigger event deploy succeeded

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-window.onload = async () => {
-  const response = await fetch("/.netlify/functions/deploy-failed");
-  const data = await response.json();
-  console.log(data);
-};
+// window.onload = async () => {
+//   const response = await fetch("/.netlify/functions/deploy-failed");
+//   const data = await response.json();
+//   console.log(data);
+// };
